002261dd52bf5edf37d5890fa974f5fdf2b9ef9e
G0WSIC <53444046+G0WSIC@users.noreply.github.com>
2019-07-29 13:46:54 -0400

    Add files via upload

---
+++ Del_RECYCLE.BIN_HomeDir.ps1
@@ -0,0 +1,3 @@
+﻿Get-ChildItem -path E:\FSData\HomeDir -Include '$RECYCLE.BIN' -Recurse -force | Remove-Item -force -Recurse
+Get-ChildItem -path E:\FSData\HomeDir -Include 'desktop.ini' -Recurse -force | Remove-Item -force -Recurse
+Get-ChildItem -path E:\FSData\HomeDir -Include '~$*.*' -Recurse -force | Remove-Item -force -Recurse
